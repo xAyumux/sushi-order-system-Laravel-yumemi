a91026d09747cdecf0081cefd6d010506619d1b7
Ayumu Fujiwara <ayumu.f.1127@gmail.com>
2023-02-14 14:48:47 +0900

    feat: Implement "/order-items" request

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CompleteOrderRequest;
 use App\Http\Requests\OrderRequest;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\OrderOption;
 
 final class OrderController extends Controller
 {
@@ -68,6 +71,34 @@ final class OrderController extends Controller
      */
     public function store(OrderRequest $request)
     {
+        $request = $request->validated();
+        $order = Order::create([
+            'customer_id' => $request->customer_id,
+            'total_price' => $request->total_price,
+            'table_number' => $request->table_number,
+        ]);
+
+        foreach ($request['order_items'] as $request_item) {
+            $order_item = OrderItem::create([
+                'order_id' => $order->id,
+                'item_id' => $request_item['item_id'],
+                'price' => $request_item['price'],
+                'amount' => $request_item['amount'],
+            ]);
+
+            // $order_option = OrderOption::create([
+            //     'order_item_id' => $order_item->id,
+            //     'option_id' => $request['order_options']['option_id'],
+            // ]);
+        }
+
+        foreach ($request['order_options'] as $request_option) {
+            $order_option = OrderOption::create([
+                'order_item_id' => $order_item->id,
+                'option_id' => $request_option['option_id'],
+            ]);
+        }
+
         $result = [
             'response' => 'Create new order',
         ];
